749fd2b878fd4cc2ca0c42a9315d934d41ba1a93
ArdiBahtiar <muhammadardibachtiar@gmail.com>
2024-01-12 10:53:03 +0700

    (done) fix bugs, and routes

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -1,8 +1,8 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Todo;
-use App\Http\Requests;
+use App\Models\Todo;
+// use App\Http\Requests;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TodoController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,24 +16,29 @@ use Illuminate\Support\Facades\Route;
 
 
 // NAMPILIN DASBOR TODO-LIST
-Route::get('/', 'TodoController@index');
-
+// Route::get('/', 'TodoController@index');
+Route::get('/', [TodoController::class, 'index']);
 
 // CREATE TODO FORM
-Route::get('/todos/create', 'TodoController@create');
+// Route::get('/todos/create', 'TodoController@create');
+Route::get('/todos/create', [TodoController::class, 'create']);
 
 
 // ADD TODO
-Route::post('/todos', 'TodoController@store');
+// Route::post('/todos', 'TodoController@store');
+Route::post('/todos', [TodoController::class, 'store']);
 
 
 // SHOW EDIT TODO
-Route::get('todos/{todo}/edit', 'TodoController@edit');
+// Route::get('todos/{todo}/edit', 'TodoController@edit');
+Route::get('todos/{todo}/edit', [TodoController::class, 'edit']);
 
 
 // UPDATE TODO
-Route::put('todos/{todo}', 'TodoController@update');
+// Route::put('todos/{todo}', 'TodoController@update');
+Route::put('todos/{todo}', [TodoController::class, 'update']);
 
 
 // DELETE TODO
-Route::delete('todos/{todo}/delete', 'TodoController@delete');
+// Route::delete('/todos/{todo}/delete', 'TodoController@delete');
+Route::delete('/todos/{todo}/delete', [TodoController::class, 'delete']);
